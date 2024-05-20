50e20513a4447c5d5bf94f003948e137678949e6
ikshit246 <ikshit.talera@gmail.com>
2024-05-20 23:25:17 +0530

    ari merge

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <main className="w-full h-full">
       {/* <LoginCustomer/> */}
+      {/* <LoginDriver/> */}
       <Homepage />
     </main>
   );

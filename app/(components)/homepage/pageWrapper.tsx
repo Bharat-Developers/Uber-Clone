ae3527fd0e0a64f558315c06d19f71be6c5e8ba3
ikshit246 <ikshit.talera@gmail.com>
2024-05-21 15:46:20 +0530

    optimised

--- app/(components)/homepage/pageWrapper.tsx
+++ app/(components)/homepage/pageWrapper.tsx
@@ -1,24 +1,22 @@
 'use client';
 
 import React, { FC, ReactNode } from 'react'
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 
 interface MyProps {
   children?: ReactNode;
 }
 
-const PageWrapper: FC<MyProps> = (props: { children: any; }) => {
+const PageWrapper: FC<MyProps> = ({ children }) => {
   return (
     <>
-      <AnimatePresence>
-        <motion.div
-          initial={{ opacity: 0, x: -50 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1 }}
-        >
-          {props.children}
-        </motion.div>
-      </AnimatePresence >
+      <motion.div
+        initial={{ opacity: 0, x: -50 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+      >
+        {children}
+      </motion.div>
     </>
   )
 }
